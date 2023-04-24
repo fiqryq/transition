04a90957a94f247530fde89ebf1c820176e9b48c
Fiqry choerudin <fiqrychoerudin48@gmail.com>
2023-04-24 19:46:23 +0700

    feat: finish site

--- src/main.js
+++ src/main.js
@@ -1,7 +1,68 @@
-const button = document.querySelector('#button');
-const text = document.querySelector('#sometext');
+import barba from '@barba/core';
+import { gsap } from 'gsap';
 
-button.addEventListener(
-  'click',
-  () => (text.innerHTML = 'This text generated form button create.')
-);
+const pageTransition = () => {
+  const timeline = gsap.timeline();
+
+  timeline.to('.page-transition', {
+    duration: 1,
+    top: '0%',
+    height: '100%',
+  });
+
+  timeline.to('.page-transition', {
+    duration: 0.8,
+    height: '100%',
+    top: '100%',
+    delay: 0.3,
+  });
+
+  timeline.set('.page-transition', {
+    top: '-100%',
+  });
+};
+
+const delay = (n) => {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+};
+
+const mainAnimation = () => {
+  const timeline = gsap.timeline();
+
+  timeline.from('.main-container', {
+    duration: 1,
+    y: 30,
+    opacity: 0,
+    stagger: {
+      amount: 0.4,
+    },
+    delay: 0.8,
+  });
+};
+
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+        pageTransition();
+        await delay(1500);
+        done();
+      },
+
+      async enter(data) {
+        mainAnimation();
+      },
+
+      async once(data) {
+        mainAnimation();
+      },
+    },
+  ],
+});
